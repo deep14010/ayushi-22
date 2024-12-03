3fe4d5a8bb3a024c858c7866d6a2fbf350498af6
deep1401 <gandhi0869@gmail.com>
2024-12-03 00:15:13 -0800

    Add fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   base: "/ayushi-22", // Add this line - replace with your repo name
 });
